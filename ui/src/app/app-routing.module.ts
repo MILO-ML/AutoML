ce0d5819b7b0541e264ca260fb8968b7c0e260c2
Samer Albahra <salbahra@gmail.com>
2022-04-03 19:21:22 -0700

    Protect auth pages

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -10,49 +10,55 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
-  },
-  {
+  }
+];
+
+if (environment.authOnly) {
+  routes.push({ path: '**', redirectTo: 'auth/sign-in' });
+} else {
+  routes.push({
     path: 'search',
     ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
     data: { authGuardPipe: redirectUnauthorizedToLogin },
     loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
   },
-  {
-    path: 'search/:dataId/:step',
-    ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
-    data: { authGuardPipe: redirectUnauthorizedToLogin },
-    loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
-  },
-  {
-    path: 'search/:dataId/job/:jobId/:step',
-    ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
-    data: { authGuardPipe: redirectUnauthorizedToLogin },
-    loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
-  },
-  {
-    path: 'search/:dataId/job/:jobId/:step/:taskId/status',
-    ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
-    data: { authGuardPipe: redirectUnauthorizedToLogin },
-    loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
-  },
-  {
-    path: 'model/:id',
-    ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
-    data: { authGuardPipe: redirectUnauthorizedToLogin },
-    loadChildren: () => import('./pages/run-model/run-model.module').then(m => m.RunModelPageModule)
-  },
-  {
-    path: 'update-license',
-    loadChildren: () => import('./pages/update-license/update-license.module').then(m => m.UpdateLicensePageModule)
-  },
-  {
-    path: 'home',
-    ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
-    data: { authGuardPipe: redirectUnauthorizedToLogin },
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
-  },
-  { path: '**', redirectTo: environment.name === 'docker' ? 'home' : 'search' }
-];
+    {
+      path: 'search/:dataId/:step',
+      ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
+      data: { authGuardPipe: redirectUnauthorizedToLogin },
+      loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
+    },
+    {
+      path: 'search/:dataId/job/:jobId/:step',
+      ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
+      data: { authGuardPipe: redirectUnauthorizedToLogin },
+      loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
+    },
+    {
+      path: 'search/:dataId/job/:jobId/:step/:taskId/status',
+      ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
+      data: { authGuardPipe: redirectUnauthorizedToLogin },
+      loadChildren: () => import('./pages/search/search.module').then(m => m.SearchPageModule)
+    },
+    {
+      path: 'model/:id',
+      ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
+      data: { authGuardPipe: redirectUnauthorizedToLogin },
+      loadChildren: () => import('./pages/run-model/run-model.module').then(m => m.RunModelPageModule)
+    },
+    {
+      path: 'update-license',
+      loadChildren: () => import('./pages/update-license/update-license.module').then(m => m.UpdateLicensePageModule)
+    },
+    {
+      path: 'home',
+      ...(environment.localUser ? {} : { canActivate: [AuthGuard] }),
+      data: { authGuardPipe: redirectUnauthorizedToLogin },
+      loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+    },
+    { path: '**', redirectTo: environment.name === 'docker' ? 'home' : 'search' }
+  );
+}
 
 @NgModule({
   imports: [
